881b64cf97fb4da5f5cf5813223d8506dedd7a55
elizabethlazear <elizabethlazear04@gmail.com>
2018-07-12 17:09:52 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,22 +1,22 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
-# def greetings(greetings="Happy Mothers Day Mommy!")
-#   puts "#{greetings}"
-# end 
-# greetings
+def greetings(greetings="Happy Mothers Day Mommy!")
+  puts "#{greetings}"
+end 
+greetings
 
-# def personalized(personalized="Happy Mothers Day Grammie!")
-#   puts "#{personalized}"
-# end
-# personalized
+def personalized(personalized="Happy Mothers Day Grammie!")
+  puts "#{personalized}"
+end
+personalized
 
 
 
-# def mothers_day(mom="Mommy")
-#   puts "Happy Mothers Day #{mom}!"
-# end
+def mothers_day(mom="Mommy")
+  puts "Happy Mothers Day #{mom}!"
+end
 
-# mothers_day
+mothers_day
 
 
 def your_name(your_name)
